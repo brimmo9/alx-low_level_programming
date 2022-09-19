c465f5523f2e484dd95f9408315481394bd1ca38
brimmo9 <brimmokhaleel@gmail.com>
2022-09-19 22:53:22 +0100

    A c program that prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * return : always 0
  */
 int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
