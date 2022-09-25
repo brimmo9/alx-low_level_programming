53729d008e06fd88c8e795d09d01f6b4485b6b99
brimmo9 <brimmokhaleel@gmail.com>
2022-09-25 23:05:02 +0100

    A program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 char low, e, q;
-e = 'e' ;
-q = 'q' ;
-for (low = 'a'; low <= 'z'; low++)
- {
-	 if (low !=e && low !=q)
-		 putchar(low);
- }
- printf("\n");
- return (0);
+e = 'e';
+q = 'q';
+for(low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+printf("\n");
+return (0);
 }
 	
