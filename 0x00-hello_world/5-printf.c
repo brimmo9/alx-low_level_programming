b34edb98cf18e5bb051d203f8df3e56ddb03f08b
brimmo9 <brimmokhaleel@gmail.com>
2022-09-25 22:35:04 +0100

    A,c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - entry point
  *
- * return : always 0
+ * Return : always 0 success
  */
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+int main(void) 
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
